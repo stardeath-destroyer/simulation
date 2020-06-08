d3b9e8371bb33cc35d04c4fdeb3a3c6ae16e0ea5
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-06-08 13:49:27 +0200

    Small refactoring

--- src/main/java/stardeath/rendering/OutputStreamRenderer.java
+++ src/main/java/stardeath/rendering/OutputStreamRenderer.java
@@ -3,9 +3,11 @@ package stardeath.rendering;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
+import stardeath.participants.Participant;
 import stardeath.participants.entities.Soldier;
 import stardeath.participants.entities.Wookie;
 import stardeath.participants.entities.empire.JumpTrooper;
+import stardeath.world.Tile;
 import stardeath.world.tiles.Elevator;
 import stardeath.world.tiles.Regular;
 import stardeath.world.tiles.Wall;
@@ -29,27 +31,27 @@ public class OutputStreamRenderer implements Renderer {
 
   @Override
   public void visitParticipant(Soldier soldier) {
-    grid[soldier.getX()][soldier.getY()] = 'S';
+    setGrid(soldier, 'S');
   }
 
   @Override
   public void visitParticipant(Wookie wookie) {
-    grid[wookie.getX()][wookie.getY()] = 'W';
+    setGrid(wookie, 'W');
   }
 
   @Override
   public void visitTile(Regular regular) {
-    grid[regular.getX()][regular.getY()] = 'r';
+    setGrid(regular, '.');
   }
 
   @Override
   public void visitTile(Elevator elevator) {
-    grid[elevator.getX()][elevator.getY()] = 'e';
+    setGrid(elevator, 'e');
   }
 
   @Override
   public void visitTile(Wall wall) {
-    grid[wall.getX()][wall.getY()] = 'w';
+    setGrid(wall, '|');
   }
 
   @Override
@@ -70,5 +72,23 @@ public class OutputStreamRenderer implements Renderer {
     }
     stream.println("--");
     stream.println(builder.toString());
+  }
+
+  /**
+   * Small helper method to set the grid given a participant and a character to represent it
+   * @param p The participant to represent
+   * @param c The character to represent the participant with
+   */
+  private void setGrid(Participant p, char c) {
+    grid[p.getX()][p.getY()] = c;
+  }
+
+  /**
+   * Small helper method to set the grid given a Tile and a character to represent it
+   * @param t The Tile to represent
+   * @param c The character to represent the tile with
+   */
+  private void setGrid(Tile t, char c) {
+    grid[t.getX()][t.getY()] = c;
   }
 }
