f7cb519381719bc22489b58c79fd4d7616fed226
Alexandre Piveteau <alexandre@piveteau.email>
2020-06-10 17:04:24 +0200

    Document the Renderer interface

--- src/main/java/stardeath/interactions/Renderer.java
+++ src/main/java/stardeath/interactions/Renderer.java
@@ -2,18 +2,47 @@ package stardeath.interactions;
 
 import stardeath.world.Floor;
 
+/**
+ * An interface describing an element that is able to render the game state to the screen and
+ * display the status to the user.
+ */
 public interface Renderer {
 
+  /**
+   * Renders a certain {@link Floor} to the screen, along with its contents.
+   *
+   * @param floor The {@link Floor} to be rendered.
+   */
   void render(Floor floor);
 
+
+  /**
+   * Registers an {@link OnRenderRequestListener} to this {@link Renderer}.
+   *
+   * @param listener The listener that's registered.
+   */
   default void registerRenderRequestListener(OnRenderRequestListener listener) {
   }
 
+  /**
+   * Unregisters an {@link OnRenderRequestListener} from this {@link Renderer}.
+   *
+   * @param listener The listener that's unregistered.
+   */
   default void unregisterRenderRequestListener(OnRenderRequestListener listener) {
   }
 
+  /**
+   * Some {@link Renderer}s have the ability to perform immediate renders, outside of what the
+   * {@link stardeath.Controller} asks. To profit from this ability, the {@link
+   * stardeath.Controller} registers itself as an {@link OnRenderRequestListener} to the {@link
+   * Renderer} to be notified of requests to draw the game state.
+   */
   interface OnRenderRequestListener {
 
+    /**
+     * This method should be called whenever a new render request is performed.
+     */
     void requestRender();
   }
 }
