e9c0f80e1f560d7c95df60070779ef40faae84e7
Alexandre Piveteau <alexandre@piveteau.email>
2020-06-12 17:43:34 +0200

    Use a CLI argument to determine the launch mode

--- src/main/java/stardeath/Main.java
+++ src/main/java/stardeath/Main.java
@@ -1,22 +1,38 @@
 package stardeath;
 
+import external.cli.CLI;
 import external.io.Decoding;
 import external.lanterna.Lanterna;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.zip.ZipFile;
 import stardeath.controller.Controller;
-import stardeath.world.World;
+import stardeath.controller.InteractionsFactory;
 
 /**
  * The main entry point of our application.
  */
 public class Main {
 
-  public static void main(String[] args) throws IOException {
-    World world = Decoding.loadWorld(new ZipFile("src/main/resources/world3.stardeath"));
+  /**
+   * Returns the {@link InteractionsFactory} that will be used to interact with the app.
+   *
+   * @param args The launch arguments.
+   * @return The retrieved {@link InteractionsFactory}
+   * @throws IOException If something went badly when creating the {@link InteractionsFactory}.
+   */
+  private static InteractionsFactory interactions(String[] args) throws IOException {
+    if (args.length == 1 && args[0].equals("--simple")) {
+      return new CLI();
+    }
+    return new Lanterna();
+  }
 
-    Controller controller = new Controller(new Lanterna(), world);
+  public static void main(String[] args) throws IOException {
+
+    Controller controller = new Controller(
+        interactions(args),
+        Decoding.loadWorld(new ZipFile("src/main/resources/world3.stardeath")
+        ));
 
     while (true) {
       controller.step();
