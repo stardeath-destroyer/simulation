193e71790b1a3ccb0ea85b2264dcf9062bdcd929
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-06-15 21:55:44 +0200

    Refactor to use ActionReducer

--- src/main/java/stardeath/controller/visitors/ChooseMove.java
+++ src/main/java/stardeath/controller/visitors/ChooseMove.java
@@ -1,11 +1,16 @@
 package stardeath.controller.visitors;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import stardeath.animates.actions.Action;
 import stardeath.animates.participants.Participant;
+import stardeath.animates.participants.entities.FlameTrooper;
+import stardeath.animates.participants.entities.JumpTrooper;
 import stardeath.animates.participants.entities.Player;
 import stardeath.animates.participants.entities.Soldier;
+import stardeath.animates.participants.entities.Trooper;
 import stardeath.animates.participants.movements.Jumper;
 import stardeath.animates.participants.movements.MovementVisitor;
 import stardeath.animates.participants.movements.Walker;
@@ -16,6 +21,7 @@ import stardeath.controller.interactions.GetDirections;
 import stardeath.controller.interactions.GetMovements;
 import stardeath.world.Vector;
 import stardeath.world.World;
+import stardeath.world.tiles.Hole;
 
 public class ChooseMove extends MovementVisitor {
 
@@ -48,6 +54,65 @@ public class ChooseMove extends MovementVisitor {
   }
 
   @Override
+  public void visitParticipant(JumpTrooper trooper) {
+    EnemyVisitor enemyVisitor = new EnemyVisitor(trooper.getFaction());
+    TileDetectionVisitor detectionVisitor = new TileDetectionVisitor() {
+
+      @Override
+      public void visitTile(Hole hole) {
+      }
+    };
+
+    world.visitVisibleAnimatesFrom(trooper, trooper.getVisibilityRange(), enemyVisitor);
+    world.visitVisibleTilesFrom(trooper, detectionVisitor);
+
+    ActionReducer reducer = baseTrooperReducer(trooper, enemyVisitor, detectionVisitor);
+    trooper.addAction(reducer.resolve());
+  }
+
+  @Override
+  public void visitParticipant(FlameTrooper trooper) {
+    visitParticipant((Trooper) trooper);
+  }
+
+  @Override
+  public void visitParticipant(Trooper trooper) {
+    EnemyVisitor enemyVisitor = new EnemyVisitor(trooper.getFaction());
+    TileDetectionVisitor detectionVisitor = new TileDetectionVisitor();
+
+    world.visitVisibleAnimatesFrom(trooper, trooper.getVisibilityRange(), enemyVisitor);
+    world.visitVisibleTilesFrom(trooper, detectionVisitor);
+
+    ActionReducer reducer = baseTrooperReducer(trooper, enemyVisitor, detectionVisitor);
+    trooper.addAction(reducer.resolve());
+  }
+
+  private ActionReducer baseTrooperReducer(
+      Trooper trooper,
+      EnemyVisitor enemyVisitor,
+      TileDetectionVisitor detectionVisitor
+  ) {
+    ActionReducer reducer = new ActionReducer();
+
+    if (enemyVisitor.getPlayer().isPresent()) {
+      reducer.add(new Pair<>(20, attackEnemy(trooper, enemyVisitor.getPlayer().get())));
+      reducer.add(new Pair<>(5, getRandomMove(trooper, detectionVisitor)));
+    }
+
+    if (! enemyVisitor.getEnemies().isEmpty()) {
+      reducer.add(new Pair<>(15, attackRandomFromList(trooper, enemyVisitor.getEnemies())));
+    }
+
+    if (! enemyVisitor.getFriends().isEmpty()) {
+      reducer.add(new Pair<>(1, attackRandomFromList(trooper, enemyVisitor.getFriends())));
+      reducer.add(new Pair<>(10, getRandomMove(trooper, detectionVisitor)));
+    }
+
+    reducer.add(new Pair<>(2, getRandomMove(trooper, detectionVisitor)));
+    return reducer;
+  }
+
+  @Override
   public void visitParticipant(Soldier soldier) {
     EnemyVisitor enemyVisitor = new EnemyVisitor(soldier.getFaction());
     TileDetectionVisitor detectionVisitor = new TileDetectionVisitor();
@@ -55,9 +120,7 @@ public class ChooseMove extends MovementVisitor {
     world.visitVisibleAnimatesFrom(soldier, soldier.getVisibilityRange(), enemyVisitor);
     world.visitVisibleTilesFrom(soldier, detectionVisitor);
 
-    Action actionToSet;
-    double random = Math.random();
-
+    /* Stay there for historic reasons, for now
     // Choose between following the player, random and attacking
     if (enemyVisitor.getPlayer().isPresent() && ! enemyVisitor.getEnemies().isEmpty()) {
       if (random < 0.2) {
@@ -65,7 +128,7 @@ public class ChooseMove extends MovementVisitor {
       } else if (random < 0.45) {
         actionToSet = followPlayer(soldier, enemyVisitor.getPlayer().get());
       } else {
-        actionToSet = attackEnemy(soldier, enemyVisitor);
+        actionToSet = attackRandomEnemy(soldier, enemyVisitor);
       }
       // follow the player or random
     } else if (enemyVisitor.getPlayer().isPresent()) {
@@ -76,13 +139,26 @@ public class ChooseMove extends MovementVisitor {
       }
       // attack !
     } else if (! enemyVisitor.getEnemies().isEmpty()) {
-      actionToSet = attackEnemy(soldier, enemyVisitor);
+      actionToSet = attackRandomEnemy(soldier, enemyVisitor);
       // default -> random
     } else {
       actionToSet = getRandomMove(soldier, detectionVisitor);
     }
+     */
+    ActionReducer reducer = new ActionReducer();
 
-    soldier.addAction(actionToSet);
+    if (enemyVisitor.getPlayer().isPresent()) {
+      reducer.add(new Pair<>(10, followPlayer(soldier, enemyVisitor.getPlayer().get())));
+      reducer.add(new Pair<>(6, getRandomMove(soldier, detectionVisitor)));
+    }
+
+    if (! enemyVisitor.getEnemies().isEmpty()) {
+      reducer.add(new Pair<>(20, attackRandomFromList(soldier, enemyVisitor.getEnemies())));
+    }
+
+    reducer.add(new Pair<>(2, getRandomMove(soldier, detectionVisitor)));
+
+    soldier.addAction(reducer.resolve());
   }
 
   private static Action getRandomMove(Participant participant, TileDetectionVisitor visitor) {
@@ -109,10 +185,12 @@ public class ChooseMove extends MovementVisitor {
     return participant.new MoveAction(participant.getPosition().directionTo(player.getPosition()));
   }
 
-  private static Action attackEnemy(Soldier soldier, EnemyVisitor visitor) {
-    List<Participant> enemies = visitor.getEnemies();
+  private static Action attackRandomFromList(Soldier soldier, List<Participant> enemies) {
     Participant enemy = enemies.get(random(0, enemies.size() - 1));
+    return attackEnemy(soldier, enemy);
+  }
 
+  private static Action attackEnemy(Soldier soldier, Participant enemy) {
     return soldier.new Fire(
         new LaserBeam(
             soldier.getPosition(),
@@ -174,5 +252,46 @@ public class ChooseMove extends MovementVisitor {
         random(-1, 1),
         random(-1, 1)
     );
+  }
+
+  private static class ActionReducer {
+    List<Pair<Integer, Action>> actionChoices;
+
+    public ActionReducer() {
+      actionChoices = new ArrayList<>();
+    }
+
+    public void add(Pair<Integer, Action> action) {
+      actionChoices.add(action);
+    }
+
+    public Action resolve() {
+      int sum = actionChoices.stream()
+          .map(pair -> pair.fst)
+          .reduce(0, Integer::sum);
+
+      List<Action> complete = new ArrayList<>();
+      actionChoices.forEach(pair -> {
+            int i = pair.fst;
+            do {
+              complete.add(pair.snd);
+              i--;
+            } while (i > 0);
+          });
+
+      Collections.shuffle(complete);
+      return complete.get(0);
+    }
+  }
+
+  // Sorry
+  private static class Pair<A,B> {
+    public A fst;
+    public B snd;
+
+    public Pair(A fst, B snd) {
+      this.fst = fst;
+      this.snd = snd;
+    }
   }
 }
