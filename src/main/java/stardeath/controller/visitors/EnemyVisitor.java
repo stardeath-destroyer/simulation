06a8cfadab7b7ff9135b28c54ea2dd6a4f6b4040
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-06-15 09:57:08 +0200

    Clean up LaserBeam

--- src/main/java/stardeath/controller/visitors/EnemyVisitor.java
+++ src/main/java/stardeath/controller/visitors/EnemyVisitor.java
@@ -1,7 +1,9 @@
 package stardeath.controller.visitors;
 
+import java.util.List;
 import java.util.Optional;
 import stardeath.animates.participants.Faction;
+import stardeath.animates.participants.Participant;
 import stardeath.animates.participants.entities.Player;
 import stardeath.animates.visitors.NoOpAnimateVisitor;
 
@@ -9,6 +11,7 @@ public class EnnemyVisitor extends NoOpAnimateVisitor {
 
   private final Faction friend;
   private Player player;
+  private List<Participant> enemies;
 
   public EnnemyVisitor(Faction friend) {
     this.friend = friend;
