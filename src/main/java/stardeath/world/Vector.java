9b8b995fccbc9a49c2d8782b47f0591a19f08978
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-06-17 18:14:45 +0200

    Add vector javadoc

--- src/main/java/stardeath/world/Vector.java
+++ src/main/java/stardeath/world/Vector.java
@@ -2,6 +2,9 @@ package stardeath.world;
 
 import java.util.Objects;
 
+/**
+ * A class representing a position or direction
+ */
 public final class Vector {
 
   public static final Vector EMPTY = new Vector(0, 0);
@@ -24,30 +27,64 @@ public final class Vector {
     this.y = y;
   }
 
+  /**
+   * Gets the x coordinate
+   * @return The x coordinate
+   */
   public int getX() {
     return x;
   }
 
+  /**
+   * Gets the y coordinate
+   * @return The y coordinate
+   */
   public int getY() {
     return y;
   }
 
+  /**
+   * Adds a vector to another. Returns a newly created vector
+   * @param other The other vector to add
+   * @return A newly created vector which is the addition of two other
+   */
   public Vector add(Vector other) {
     return new Vector(this.x + other.x, this.y + other.y);
   }
 
-  public Vector sub(Vector position) {
-    return add(position.inverse());
+  /**
+   * Subtracts a vector to another. Returns a newly created vector.
+   * The subtraction is equivalent to vector1.add(vector2.inverse())
+   * @param other The vector to subtract
+   * @return A newly created vector which is the subtraction of two other
+   */
+  public Vector sub(Vector other) {
+    return add(other.inverse());
   }
 
+  /**
+   * Returns a newly created vector based on the current one, but with a new x coordinate
+   * @param x The x coordinate to set
+   * @return A newly created vector with its x coordinate being set by the argument
+   */
   public Vector withX(int x) {
     return new Vector(x, this.y);
   }
 
+  /**
+   * Returns a newly created vector based on the current one, but with a new y coordinate
+   * @param y The y coordinate to set
+   * @return A newly created vector with its y coordinate being set by the argument
+   */
   public Vector withY(int y) {
     return new Vector(this.x, y);
   }
 
+  /**
+   * Returns the inverse of a vector. Equivalent to multiplying the vector by -1
+   * Technically, it's the opposite of a vector...
+   * @return A newly created vector which is the opposite of the calling one
+   */
   public Vector inverse() {
     return new Vector(-this.x, -this.y);
   }
@@ -67,6 +104,11 @@ public final class Vector {
     );
   }
 
+  /**
+   * Get the distance to another vector
+   * @param other The other vector to get the distance to
+   * @return The distance in steps to another vector
+   */
   public int distanceTo(Vector other) {
     int deltaX = getX() - other.getX();
     int deltaY = getY() - other.getY();
@@ -91,6 +133,12 @@ public final class Vector {
     return Objects.hash(x, y);
   }
 
+  /**
+   * Clamps the x coordinate of a vector between the min and the max
+   * @param min Lower bound
+   * @param max Upper bound
+   * @return A new vector bounded in x by min and max
+   */
   public Vector clampX(int min, int max) {
     return new Vector(
         clamp(this.x, min, max),
@@ -98,6 +146,12 @@ public final class Vector {
     );
   }
 
+  /**
+   * Clamps the y coordinate of a vector between the min and the max
+   * @param min Lower bound
+   * @param max Upper bound
+   * @return A new vector bounded in y by min and max
+   */
   public Vector clampY(int min, int max) {
     return new Vector(
         this.x,
