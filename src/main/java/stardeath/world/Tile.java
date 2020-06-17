c46f6917364323e578f31017821310be7df4a3a4
Guy-Laurent Subri <guy-laurent@subri.ch>
2020-06-17 18:22:53 +0200

    Add tile javadoc

--- src/main/java/stardeath/world/Tile.java
+++ src/main/java/stardeath/world/Tile.java
@@ -3,6 +3,9 @@ package stardeath.world;
 import stardeath.Entity;
 import stardeath.world.visitors.TileVisitor;
 
+/**
+ * Represents a Tile
+ */
 public abstract class Tile extends Entity {
 
   private boolean discovered;
@@ -14,17 +17,32 @@ public abstract class Tile extends Entity {
     this.opaque = opaque;
   }
 
+  /**
+   * Discovers the tile to the player
+   */
   public final void unveil() {
     this.discovered = true;
   }
 
+  /**
+   * Returns true if the tile has been discovered by the player
+   * @return True if the tile has been discovered by the player
+   */
   public final boolean isDiscovered() {
     return discovered;
   }
 
+  /**
+   * Returns true if the tile is opaque (i.e. we cannot see through it)
+   * @return True if the tile is opaque (i.e. we cannot see through it)
+   */
   public final boolean isOpaque() {
     return opaque;
   }
 
+  /**
+   * Accepts a given visitor
+   * @param visitor The visitor that will be accepted
+   */
   public abstract void accept(TileVisitor visitor);
 }
